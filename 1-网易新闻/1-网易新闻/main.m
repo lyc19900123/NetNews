18dc601cfefaa9b40000718003152aa1f9f76cab
lyc <285767005@qq.com>
2017-03-10 18:48:54 +0800

    网易新闻预备

---
+++ 1-网易新闻/1-网易新闻/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1-网易新闻
+//
+//  Created by 梁永超 on 17/3/10.
+//  Copyright © 2017年 lyc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
